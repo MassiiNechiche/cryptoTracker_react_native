f69b1a83358cef750ccc6cc22592283705041aad
Massii <massinechiche@gmail.com>
2022-01-12 17:27:08 +0100

    Render Flat list

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native'
+import ListItem from './components/ListItem'
+import { SAMPLE_DATA } from './assets/data/sampleData'
+
+const ListHeader = () => (
+    <>
+        <View style={styles.titleWrapper}>
+            <Text style={styles.title}>Markets</Text>
+        </View>
+        <View style={styles.line}></View>
+    </>
+)
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <FlatList
+                keyExtractor={(item) => item.id}
+                data={SAMPLE_DATA}
+                renderItem={({ item }) => (
+                    <ListItem
+                        name={item.name}
+                        symbol={item.symbol}
+                        currentPrice={item.current_price}
+                        priceChangePercentage7d={
+                            item.price_change_percentage_7d_in_currency
+                        }
+                        logoUrl={item.image}
+                        onPress={() => openModal(item)}
+                    />
+                )}
+                ListHeaderComponent={<ListHeader />}
+            />
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+    titleWrapper: {
+        marginTop: 30,
+        marginHorizontal: 16,
+    },
+    line: {
+        height: StyleSheet.hairlineWidth,
+        backgroundColor: 'grey',
+        marginHorizontal: 16,
+        marginTop: 16,
+    },
+})
